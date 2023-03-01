69fe94fc96a1ad11bc2490840471b144db721cc4
TinaByrd <thuaneoliveira1@gmail.com>
2023-03-01 13:22:50 -0300

    uso do equals e operadores de comparação

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,9 +1,24 @@
 public class Operadores {
     public static void main(String[] args) {
-        boolean variavel = true;
+        String nomeUm = "Maria";
+        String nomeDois = new String("Maria");
+      
+        System.out.println(nomeUm.equals(nomeDois)); /*para string se usa o equals para comparar */
 
-        variavel = !variavel;
 
-        System.out.println(variavel);
+        int numero1 = 1;
+        int numero2 = 2;
+
+        boolean simNao = numero1 == numero2;
+
+        System.out.println("numero1 é igual a numero2? " + simNao);
+
+        simNao = numero1 != numero2;
+
+        System.out.println("numero1 é diferente de numero2? " + simNao);
+
+        simNao = numero1 > numero2;
+
+        System.out.println("numero1 é maior que numero2? " + simNao);
     }
 }
